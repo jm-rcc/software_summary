3dadec91b520bfb2ca9367207a9653b3485ad888
joshm <j.maloney@uq.edu.au>
2026-06-30 16:54:20 +1000

    Modules data
    Change-Id: Iae6e97d0a183a4c8d65c8f948435dc035d58d0bb

--- scripts/prepare_lists.py
+++ scripts/prepare_lists.py
@@ -7,6 +7,25 @@ labels = ["Description:", "Homepage:", "URL:", "Extensions:"]
 
 moduledata = {}
 
+with open(f"{output_dir}/module_index.json") as f:
+    j = json.load(f)
+    for k, v in j.items():
+        moduledata[k] = {"":"", "Description:":"", "Homepage:":"", "URL:":"", "Extensions:":"", "Skip":""}
+        desc = v["description"]
+        lines = desc.split('\n')
+        caught_label = ""
+        for line in lines:
+            if line.strip() == "": continue
+            for label in labels:
+                if line.startswith(label):
+                    if len(moduledata[k][label]) > 0:
+                        # Skip if that label has already been captured.
+                        caught_label = "Skip"
+                        continue
+                    caught_label = label
+                    line = line[len(label):]
+            moduledata[k][caught_label] += f"{line}\n"
+
 new_json = {}
 export_labels = ["Description:", "Homepage:", "Extensions:", ""]
 for i, j in moduledata.items():
