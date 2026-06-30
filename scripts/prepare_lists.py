c0bb7d72593a5ff49d399eedc345e6b71d699711
joshm <j.maloney@uq.edu.au>
2026-06-30 16:45:22 +1000

    Change output
    Change-Id: Ifff703408b61d25968fabfd9f7f7d4d58ea1c596

--- scripts/prepare_lists.py
+++ scripts/prepare_lists.py
@@ -7,26 +7,6 @@ labels = ["Description:", "Homepage:", "URL:", "Extensions:"]
 
 moduledata = {}
 
-print(f"{output_dir}/module_index.json")
-with open(f"{output_dir}/module_index.json") as f:
-    j = json.load(f)
-    for k, v in j.items():
-        moduledata[k] = {"":"", "Description:":"", "Homepage:":"", "URL:":"", "Extensions:":"", "Skip":""}
-        desc = v["description"]
-        lines = desc.split('\n')
-        caught_label = ""
-        for line in lines:
-            if line.strip() == "": continue
-            for label in labels:
-                if line.startswith(label):
-                    if len(moduledata[k][label]) > 0:
-                        # Skip if that label has already been captured.
-                        caught_label = "Skip"
-                        continue
-                    caught_label = label
-                    line = line[len(label):]
-            moduledata[k][caught_label] += f"{line}\n"
-
 new_json = {}
 export_labels = ["Description:", "Homepage:", "Extensions:", ""]
 for i, j in moduledata.items():
@@ -34,8 +14,6 @@ for i, j in moduledata.items():
     for k in export_labels:
         if len(j[k].strip()):
             new_json[i][k] = j[k].strip()
-    #if "Description:" not in new_json[i]:
-    #    new_json[i]["Description:"] = j[""]
 
 print(f"{output_dir}/module_index.json")
 with open(f"{output_dir}/new_output.json", "w") as f:
